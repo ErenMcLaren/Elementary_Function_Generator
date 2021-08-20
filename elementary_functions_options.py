d96e5d792b1dc38dac0b58b2b52a454c467047df
Eren McLaren <70293193+ErenMcLaren@users.noreply.github.com>
2021-08-19 22:23:05 -0500

    What are possible elementary functions?
    Still not quite done yet. Leaving out lots of stuff. This file is mostly for reference.

---
+++ elementary_functions_options.py
@@ -0,0 +1,44 @@
+    # https://en.wikipedia.org/wiki/Elementary_function
+LIST_OF_ELEMENTARY_FUNCTIONS = [
+    "constant",
+    "power",
+    "square_root",
+    "nth_root",
+    "exponential",
+    "logarithmic",
+    "trigonometric",
+    "hyperbolic",
+    "inverse_trigonometric"
+]
+
+LIST_OF_TRIGONOMETRIC_FUNCTIONS = [
+    "sine",
+    "cosine",
+    "tangent",
+    "secant",
+    "cosecant",
+    "cotangent"
+]
+
+LIST_OF_INVERSE_TRIGONOMETRIC_FUNCTIONS = [
+    "arcsine",
+    "arccosine",
+    "arctangent",
+    "arcsecant",
+    "arccosecant",
+    "arccotangent"
+]
+
+LIST_OF_HYPERBOLIC_TRIGONOMETRIC_FUNCTIONS = [
+    "sinh",
+    "cosineh",
+    "tangenth",
+    "secanth",
+    "cosecanth",
+    "cotangenth"
+]
+
+LIST_OF_POSSIBLE_FUNCTIONAL_FORMS = [
+    "rational",
+    "product"
+]
